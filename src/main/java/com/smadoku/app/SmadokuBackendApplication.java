0d18a21663309d8de0d95028f25b5bb4cefc04d1
YANGEUNOK\82103 <tlawnsgh1@naver.com>
2023-06-24 22:02:16 +0900

    first commit

---
+++ src/main/java/com/smadoku/app/SmadokuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smadoku.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmadokuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmadokuBackendApplication.class, args);
+	}
+
+}
